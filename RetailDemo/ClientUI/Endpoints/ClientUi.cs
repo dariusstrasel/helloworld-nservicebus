43d401a8b11d6ead465b0b5647c7dd388374f84c
EMA\dstrasel <dstrasel@emoneyadvisor.com>
2018-07-25 13:16:16 -0400

    Swap ClientUI endpoint to read from STDIN.

--- RetailDemo/ClientUI/Endpoints/ClientUi.cs
+++ RetailDemo/ClientUI/Endpoints/ClientUi.cs
@@ -21,8 +21,8 @@ namespace ClientUI.Endpoints
             var endpointInstance = await Endpoint.Start(endpointConfiguration)
                 .ConfigureAwait(false);
 
-            Console.WriteLine("Press Enter to exit...");
-            Console.ReadLine();
+            await RunLoop(endpointInstance)
+                .ConfigureAwait(false);
 
             await endpointInstance.Stop()
                 .ConfigureAwait(false);
