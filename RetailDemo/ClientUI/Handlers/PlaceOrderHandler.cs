08af2fd39742561f44947cb39937e493da511bbf
EMA\dstrasel <dstrasel@emoneyadvisor.com>
2018-07-25 11:24:35 -0400

    Add PlaceOrder message and handler.

---
+++ RetailDemo/ClientUI/Handlers/PlaceOrderHandler.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace ClientUI.Handlers
+{
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        private static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+        
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+            
+            return Task.CompletedTask;
+        }
+    }
+}
